790728b987df94c6c651aa77cdc8f5e1e6893739
marios-stam <mariosstamatopoulos1@gmail.com>
2021-12-13 17:59:04 +0200

    Made separate coll checking planner a class

--- scripts/RB_planning_sep_coll_check.py
+++ scripts/RB_planning_sep_coll_check.py
@@ -23,140 +23,130 @@ from math import pi
 import tf
 from sep_collision_checking import *
 
-start_pos = [2, 2, 0]
-start_euler = [0, 0, 0]
-
-goal_pos = [4, -2, 2]
-goal_euler = [0, 0, 0]
-
-
-def get_start_goal(space, transform=False):
-    # start
-    start_pose = PoseStamped()
-    start_pose.pose.position = Point(start_pos[0], start_pos[1], start_pos[2])
-    q = tf.quaternion_from_euler(
-        start_euler[0], start_euler[1], start_euler[2])
-    start_pose.pose.orientation = Quaternion(q[0], q[1], q[2], q[3])
-
-    # goal
-    goal_pose = PoseStamped()
-    goal_pose.pose.position = Point(goal_pos[0], goal_pos[1], goal_pos[2])
-    q = tf.quaternion_from_euler(goal_euler[0], goal_euler[1], goal_euler[2])
-    goal_pose.pose.orientation = Quaternion(q[0], q[1], q[2], q[3])
-
-    if transform:
-        start_pose = transform(start_pose)
-        goal_pose = transform(goal_pose)
-
-    start_pose = start_pose.pose
-    goal_pose = goal_pose.pose
-
-    # define start state
-    print("Space:", space)
-    start = ob.State(space)
-    start().setX(start_pose.position.x)
-    start().setY(start_pose.position.y)
-    start().setZ(start_pose.position.z)
-    start().rotation().x = start_pose.orientation.x
-    start().rotation().y = start_pose.orientation.y
-    start().rotation().z = start_pose.orientation.z
-    start().rotation().w = start_pose.orientation.w
-
-    goal = ob.State(space)
-    goal().setX(goal_pose.position.x)
-    goal().setY(goal_pose.position.y)
-    goal().setZ(goal_pose.position.z)
-    goal().rotation().x = goal_pose.orientation.x
-    goal().rotation().y = goal_pose.orientation.y
-    goal().rotation().z = goal_pose.orientation.z
-    goal().rotation().w = goal_pose.orientation.w
-
-    # return the start & goal states
-    return start, goal
-
-
-def get_bounds():
-    bounds = ob.RealVectorBounds(3)
-    bounds.low[0] = -6.0
-    bounds.low[1] = -6.0
-    bounds.low[2] = -4
-
-    bounds.high[0] = 8
-    bounds.high[1] = 8
-    bounds.high[2] = 10
-
-    # bounds.setLow(-10)
-    # bounds.setHigh(10)
-
-    return bounds
-
-
-def isStateValid(state):
-    # Some arbitrary condition on the state (note that thanks to
-    # dynamic type checking we can just call getX() and do not need
-    # to convert state to an SE2State.)
-    pos = [state.getX(), state.getY(), state.getZ()]
-    q = [state.rotation().x, state.rotation().y,
-         state.rotation().z, state.rotation().w]
-
-    checker.set_robot_transform(pos, q)
-
-    is_valid = not checker.collision_check()
-    # print("is_valid:", is_valid)
-
-    return is_valid
-
-
-def save_path(path, file_name="path.txt"):
-    # save the path
-    print("Saving path to %s" % file_name)
-    text_file = open(file_name, "w")
-    n = text_file.write(path.printAsMatrix())
-    text_file.close()
-
-
-def planWithSimpleSetup():
-    # create an SE2 state space
-    space = ob.SE3StateSpace()
-    # set lower and upper bounds
-    bounds = get_bounds()
-    space.setBounds(bounds)
-
-    # create a simple setup object
-    ss = og.SimpleSetup(space)
-
-    ss.setStateValidityChecker(ob.StateValidityCheckerFn(isStateValid))
-
-    start, goal = get_start_goal(space)
-    ss.setStartAndGoalStates(start, goal)
-
-    # choose planner
-    # planner = og.PRM(ss.getSpaceInformation())
-    # ss.setPlanner(planner)
-    # ss.setup()
-
-    # this will automatically choose a default planner with
-    # default parameters
-
-    solved = ss.solve(5.0)
-    if solved:
-        print("Found solution...")
-        # try to shorten the path
-        ss.simplifySolution()
-        # print the simplified path
-        path = ss.getSolutionPath()
-        path.interpolate(50)
-
-        save_path(path)
-    else:
-        print("No solution found")
-
 
 class PlannerSepCollision:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, coll_checker) -> None:
+        self.coll_checker = coll_checker
+
+        self.space = ob.SE3StateSpace()
+        # set lower and upper bounds
+        self.set_bounds()
+
+        # create a simple setup object
+        self.ss = og.SimpleSetup(self.space)
+        self.ss.setStateValidityChecker(
+            ob.StateValidityCheckerFn(PlannerSepCollision.isStateValid))
+
+        # self.set_start_goal(self.space)
+
+        # choose planner
+        # planner = og.PRM(ss.getSpaceInformation())
+        # ss.setPlanner(planner)
+        # ss.setup()
+
+    def set_bounds(self):
+        bounds = ob.RealVectorBounds(3)
+        bounds.low[0] = -6.0
+        bounds.low[1] = -6.0
+        bounds.low[2] = -4
+
+        bounds.high[0] = 8
+        bounds.high[1] = 8
+        bounds.high[2] = 10
+
+        # bounds.setLow(-10)
+        # bounds.setHigh(10)
+        self.space.setBounds(bounds)
+
+        return bounds
+
+    def save_path(self, file_name="path.txt"):
+        # save the path
+        print("Saving path to %s" % file_name)
+        text_file = open(file_name, "w")
+        n = text_file.write(self.path.printAsMatrix())
+        text_file.close()
+
+    def set_start_goal(self, start_pos, goal_pos, transform=False):
+        # start
+        start_pose = PoseStamped()
+        start_pose.pose.position = Point(
+            start_pos[0], start_pos[1], start_pos[2])
+        start_pose.pose.orientation = Quaternion(0, 0, 0, 1)
+
+        # goal
+        goal_pose = PoseStamped()
+        goal_pose.pose.position = Point(goal_pos[0], goal_pos[1], goal_pos[2])
+        goal_pose.pose.orientation = Quaternion(0, 0, 0, 1)
+
+        if transform:
+            start_pose = transform(start_pose)
+            goal_pose = transform(goal_pose)
+
+        start_pose = start_pose.pose
+        goal_pose = goal_pose.pose
+
+        # define start state
+        print("Space:", self.space)
+        start = ob.State(self.space)
+        start().setX(start_pose.position.x)
+        start().setY(start_pose.position.y)
+        start().setZ(start_pose.position.z)
+        start().rotation().x = start_pose.orientation.x
+        start().rotation().y = start_pose.orientation.y
+        start().rotation().z = start_pose.orientation.z
+        start().rotation().w = start_pose.orientation.w
+
+        goal = ob.State(self.space)
+        goal().setX(goal_pose.position.x)
+        goal().setY(goal_pose.position.y)
+        goal().setZ(goal_pose.position.z)
+        goal().rotation().x = goal_pose.orientation.x
+        goal().rotation().y = goal_pose.orientation.y
+        goal().rotation().z = goal_pose.orientation.z
+        goal().rotation().w = goal_pose.orientation.w
+
+        self.ss.setStartAndGoalStates(start, goal)
+        # return the start & goal states
+        return start, goal
+
+    def plan(self):
+        #
+
+        # this will automatically choose a default planner with
+        # default parameters
+
+        solved = self.ss.solve(5.0)
+        if solved:
+            print("Found solution...")
+            # try to shorten the path
+            self.ss.simplifySolution()
+            # print the simplified path
+            path = self.ss.getSolutionPath()
+            path.interpolate(50)
+
+            self.path = path
+            self.save_path()
+        else:
+            print("No solution found")
+
+    def isStateValid(state):
+        # Some arbitrary condition on the state (note that thanks to
+        # dynamic type checking we can just call getX() and do not need
+        # to convert state to an SE2State.)
+        pos = [state.getX(), state.getY(), state.getZ()]
+        q = [state.rotation().x, state.rotation().y,
+             state.rotation().z, state.rotation().w]
+
+        checker.set_robot_transform(pos, q)
+
+        is_valid = not checker.collision_check()
+        # print("is_valid:", is_valid)
+
+        return is_valid
 
 
+"""
 def planTheHardWay():
     # create an SE2 state space
     space = ob.SE2StateSpace()
@@ -168,7 +158,7 @@ def planTheHardWay():
     # construct an instance of space information from this state space
     si = ob.SpaceInformation(space)
     # set state validity checking for this space
-    si.setStateValidityChecker(ob.StateValidityCheckerFn(isStateValid))
+    si.setStateValidityChecker(ob.StateValidityCheckerFn(PlaisStateValid))
     # create a random start state
     start = ob.State(space)
     start.random()
@@ -198,7 +188,7 @@ def planTheHardWay():
         print("Found solution:\n%s" % path)
     else:
         print("No solution found")
-
+"""
 
 if __name__ == "__main__":
     checker = SepCollisionChecking()
@@ -206,4 +196,9 @@ if __name__ == "__main__":
     checker.set_robot_transform([4, -2, 2], [0, 0, 0, 1])
     # checker.visualize()
 
-    planWithSimpleSetup()
+    planner = PlannerSepCollision(checker)
+
+    start = [2, 2, 0]
+    goal = [4, -2, 2]
+    planner.set_start_goal(start, goal)
+    planner.plan()
